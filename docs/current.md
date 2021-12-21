d725896c32e7e2dc5aa6e5227fcca07a0e7b695c
Dumbo-programmer <72726301+Dumbo-programmer@users.noreply.github.com>
2021-12-21 15:45:01 +0600

    Add files via upload

---
+++ docs/current.md
@@ -0,0 +1,8 @@
+#symbols
+'#' means number constant
+
+##instructions
+
+*`PUSH` - push an 8 bit number into the stack
+*`ADD`  - pops last two numbers off
+*`HLT`  - pops stack and returns exit code
---
+++ include/util.h
@@ -0,0 +1,16 @@
+#ifndef KREEP_UTIL_H
+#define KREEP_UTIL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include "bytebuffer.h"
+
+char* read_ascii_file(const char* path);
+
+uint8_t* read_binary_file(const char* path);
+void write_binary_file(const char* path, ByteBuffer* bb);
+
+uint16_t read16(uint8_t* buffer, uint32_t index);
+uint32_t read32(uint8_t* buffer, uint32_t index);
+
+#endif 
+//
