6460dbc8d9ccedf80098f66c5f10ea0b138379e4
Dumbo-programmer <72726301+Dumbo-programmer@users.noreply.github.com>
2021-12-21 15:41:39 +0600

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "../include/util.h"
+//kreep compie filename.kr
 
 int main(int argc, char** argv){
-    printf("hello running\n");
+    if(argc <3){
+        printf("Too less arguments");
+        return 1;
+    }
+
+    if(strcmp(argv[1], "compile") == 0){
+        char* source = read_ascii_file(argv[2]);
+        printf("$s\n, source");
+        free(source);
+    }
     return 0;
 }
